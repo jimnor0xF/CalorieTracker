759adb86a80ab792be2eff4aa657573c0fda67f4
jimnor0xF <xanthos444@gmail.com>
2020-02-04 15:02:52 +0100

    Title in center

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav style={{ marginBottom: '30px' }} className='blue'>
         <div className="nav-wrapper">
-          <a href="#" class="brand-logo">CalorieTracker</a>
+          <a href="#" class="brand-logo center">CalorieTracker</a>
           <ul id="nav-mobile" class="right hide-on-med-and-down">
             <li><a href="#">Register</a></li>
             <li><a href="#">Login</a></li>
